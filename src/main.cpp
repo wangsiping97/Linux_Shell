2ac8a07b6ac85808e2119a8e099ed6cdcb488dc7
wangsiping97 <wangsiping1997@gmail.com>
2019-07-01 14:16:23 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,50 +17,51 @@ using std::cout;
 using std::string;
 using std::vector;
 
-enum TYPE {
-    foreground,
-    background
-};
-
-class Jobs {
-private: 
-    pid_t pid;
-    TYPE type;
-};
-
 class Shell {
 private: 
-    istream& in;
-    ostream& out;
-    vector<string> vcmd;
-    string workPath; // 工作目录
-    string rootPath; // 根目录
-    string name;
-    bool background;
-    int count;
+    istream& in; // input
+    ostream& out; // output
+    vector<string> vcmd; // vector of command
+    string workPath; // work path of the program
+    string defaultPath; // default path
+    string name; // name of the user
+    bool background; // is the command executed in background
+    int count; // number of background commands
+    string cmdstring; // the command read from the keyboard
 private: 
-    void split(string& command, char flag);
-    bool parseCommand();
-    bool execute();
-    string getPath();
-    void cd(vector<string>& tempcmd);
-    void help();
-    void bye();
-    int myexecvp(vector<string> tempcmd);
+    void split(string& command, char flag); // split the command/path
+    bool parseCommand(); // parse the command
+    void execute(); // execute the command
+    string getPath(); // get 
+    int cd(vector<string>& tempcmd); // built-in cd
+    void bye(); // built-in bye
+    string rebuildCommand();
+    bool exec_cd();
+    int exec_command(string& cmdstring);
 public: 
-    Shell(istream& _in, ostream& _out);
-    string getDirName();
-    void init();
-    void run();
+    Shell(istream& _in, ostream& _out); // construct function
+    string getDirName(); // get the name of the directory in the workpath
+    void init(); // init the shell
+    void run(); // run the shell
 };
 
-
+/**
+ * Contruct function
+ * Initialize the background and count
+ */
 Shell::Shell(istream& _in, ostream& _out):in(_in), out(_out) {
     vcmd.clear();
+    cmdstring.clear();
     background = false;
     count = 0;
 }
 
+/**
+ * Init function
+ * Init the shell program with an UI
+ * Initialize the work path as well as the default path
+ * Switch the work path to the default path
+ */ 
 void Shell::init() {
     system("clear");
     out << string(5, '\n');
@@ -68,18 +69,120 @@ void Shell::init() {
     out << string(3, '\n');
     out << "                            by Siping Wang @ THU                        \n";
     out << string(5, '\n');
+    // get the workPath 
     workPath = getcwd(NULL, 0);
     split(workPath, '/');
+    // get the user's name
     if (vcmd.size() == 2) name = vcmd[1];
     else name = vcmd[2];
+    // cd to the user's default path
     if (chdir(("/" + vcmd[1] + "/" + name).c_str()) != 0) {
         if (chdir(("/" + name).c_str()) != 0) {
             perror("ERROR");
             EXIT_FAILURE;
         }
     }
-    rootPath = getcwd(NULL, 0);
-    workPath = rootPath;
+    // get the root(default) path
+    defaultPath = getcwd(NULL, 0);
+    // change the work path
+    workPath = defaultPath;
+}
+
+/**
+ * Built-in "bye" function
+ * Exit the program with a bye-string
+ */ 
+void Shell::bye() {
+    out << "MyShell: Thanks for using : )\n";
+}
+
+/**
+ * Built-in cd function
+ * If the command is just "cd", switch the work path to the defaultPath
+ * If the command is "cd + [path]", switch the work path to [path]
+ * Then, consider if the command is to be executed in the background
+ * If not, change the work path of this program
+ */ 
+int Shell::cd(vector<string>& tempcmd) {
+    if (tempcmd.size() == 1) { // 默认目录
+        if (chdir((defaultPath).c_str()) != 0) {
+            perror("MyShell");
+            return -1;
+        }
+    }
+    else {
+        if (chdir(tempcmd[1].c_str()) != 0) {
+            perror("MyShell");
+            return -1;
+        }
+    }
+    if (background == false) {
+        workPath = getcwd(NULL, 0);
+    }
+    return 0;
+}
+
+/**
+ * A function that executes the cd-series commands in the program
+ * First decide whether there are cd-series commands in the command
+ * Then decide whether the command is with an argument
+ * Then execute the command and update the `vcmd`
+ */ 
+bool Shell::exec_cd() {
+    int sz = vcmd.size();
+    if (sz == 0) return true;
+    vector<string> tempcmd;
+    tempcmd.clear();
+    if (vcmd[0] == "cd") {
+        if (sz == 1) {
+            tempcmd.push_back(vcmd[0]);
+            if(cd(tempcmd)) return false;
+            vcmd.clear();
+        }
+        else if (vcmd[1] == "&&") {
+            tempcmd.push_back(vcmd[0]);
+            if(cd(tempcmd)) return false;
+            vcmd.erase(vcmd.begin());
+            vcmd.erase(vcmd.begin());
+        }
+        else if (sz == 2) {
+            tempcmd.push_back(vcmd[0]);
+            tempcmd.push_back(vcmd[1]);
+            if(cd(tempcmd)) return false;
+            vcmd.clear();
+        }
+        else {
+            tempcmd.push_back(vcmd[0]);
+            tempcmd.push_back(vcmd[1]);
+            if(cd(tempcmd)) return false;
+            vcmd.erase(vcmd.begin());
+            vcmd.erase(vcmd.begin());
+            vcmd.erase(vcmd.begin()); // &&
+        }
+    }
+    return true;
+}
+
+int Shell::exec_command(string& cmdstring) {
+    pid_t pid;
+    int status;
+    if(cmdstring.empty()){
+        return (1);
+    }
+
+    if((pid = fork())<0){
+        status = -1;
+    }
+    
+    else if(pid == 0){
+        execl("/bin/sh", "MyShell", "-c", cmdstring.data(), (char *)0);
+    }
+    else{
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return status;
 }
 
 string Shell::getDirName() {
@@ -96,98 +199,28 @@ void Shell::split(string& command, char flag) {
         vcmd.push_back(temp);
 }
 
-bool Shell::execute() {
-    pid_t pid;
-    int status;
-    pid = fork();
-    if (pid == 0) {
-        // check if there are multiple commands in the line, then execute all the commands.
-        vector<string> tempcmd;
-        int res;
-        int i = 0;
-        int sz = vcmd.size();
-        do {
-            tempcmd.clear();
-            while (i < sz && vcmd[i] != "&&") {
-                tempcmd.push_back(vcmd[i]);
-                i++;
-            }
-            res = myexecvp(tempcmd);
-            if (res == -1) {
-                perror("MyShell");
-                return false;
-            }
-        } while (i <= sz && !res);
+string Shell::rebuildCommand() {
+    string command;
+    command.clear();
+    if (!vcmd.empty() && vcmd[0] == "&&") {
+        vcmd.erase(vcmd.begin());
     }
-    else if (pid < 0) {
-        perror("MyShell");
-        return false;
+    while (!vcmd.empty() && vcmd[0] != "cd" && vcmd[0] != "&&") {
+        command += vcmd[0];
+        command += " ";
+        vcmd.erase(vcmd.begin());
     }
-    else {
-        do {
-            if (background == false)
-                waitpid(pid, &status, WUNTRACED);
-            else {
-                out << "[" << count << "]" << "(" << pid << ")\n"; 
-            }
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    return true;
-}
-
-void Shell::cd(vector<string>& tempcmd) {
-    if (tempcmd.size() == 1) { // 默认去根目录
-        if (chdir((rootPath).c_str()) != 0) {
-            perror("MyShell");
-        }
-    }
-    else {
-        if (chdir(tempcmd[1].c_str()) != 0) {
-            perror("MyShell");
-        }
-    }
-    if (background == false)
-        workPath = getcwd(NULL, 0);
-}
-
-void Shell::help() {
-    out << "These shell commands are defined internally.\n";
-    out << "Type program names and arguments, then hit enter.\n";
-    out << "Use \'man -k\' or \'info\' to find out more about commands not in this list.\n";
-}
-
-void Shell::bye() {
-    out << "MyShell: Thanks for using ~\n";
-}
-
-int Shell::myexecvp(vector<string> tempcmd) {
-    int argc = tempcmd.size();
-    if (argc == 0) // command starts with "&&"
-    {
-        return 0;
-    }
-    if (tempcmd[0] == "cd" && tempcmd.size() <= 2) {
-        cd(tempcmd);
-        return 0;
-    }
-    if (tempcmd[0] == "help") {
-        help();
-        return 0;
-    }
-    if (tempcmd[0] == "bye" && tempcmd.size() == 1) {
-        bye();
-        return 0;
-    }
-    char ** args = new char* [argc + 1];
-    for (int i = 0; i < argc; ++i) {
-        args[i] = (char*)tempcmd[i].data();
-    }
-    args[argc] = NULL;
-    return execvp(args[0], args);
+    return command;
 }
 
 bool Shell::parseCommand() {
-    if (vcmd.empty()) return true;
+    if (vcmd.empty()) {
+        return true;
+    }
+    if (vcmd[0] == "bye" || vcmd[0] == "exit") {
+        bye();
+        return false;
+    }
     if (vcmd.back() == "*") {
         background = true;
         vcmd.pop_back();
@@ -196,36 +229,53 @@ bool Shell::parseCommand() {
     else {
         background = false;
     }
-    if (vcmd[0] == "bye" && vcmd.size() == 1 && background == false) {
-        bye();
-        return false;
+    execute();
+    return true;
+}
+
+void Shell::execute() {
+    if (background == false) {
+        while (!vcmd.empty()) {
+            if (!exec_cd()) return;
+            string command = rebuildCommand();
+            if (!command.empty()) exec_command(command);
+        } 
     }
-    if (vcmd.empty()) // An empty command was entered.
-    {
-        if (background == false)
-            return true;
-        if (background == true) {
-            out << "MyShell: syntax error near unexpected token `*'\n";
-            return true;
+    else {
+        pid_t pid;
+        int status;
+        pid = fork();
+        if (pid == 0) {
+            cmdstring.erase(cmdstring.find_last_not_of("*") + 1);
+            while (!vcmd.empty()) {
+                if (!exec_cd()) return;
+                string command = rebuildCommand();
+                if (!command.empty()) exec_command(command);
+            }
+            out << "\n[" << count << "]" <<"+  Done\t" << cmdstring << "\n"; 
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0) {
+            perror("MyShell");
+        }
+        else {
+            out << "[" << count << "]" << "(" << pid << ")\n"; 
         }
     }
-    if (vcmd[0] == "&&") {
-        out << "MyShell: syntax error near unexpected token `&&'\n";
-        return true;
-    }
-    return execute();
 }
 
 void Shell::run() {
-    string line;
     string dirName;
     int status;
     do {
         dirName = getDirName();
         if (dirName == name) dirName = "~";
+        // output the prompt
         out << "MyShell:" << dirName << " " << name << "$ ";
-        getline(cin, line);
-        split(line, ' ');
+        // read the command from the keyboard
+        getline(cin, cmdstring);
+        // split the command with ' '
+        split(cmdstring, ' ');
         status = parseCommand();
     } while (status);
 }
@@ -234,13 +284,11 @@ void sigint_handler(int sig)
 {
 	int olderrno = errno;
 	pid_t fg_pid = getpid();
-	if(fg_pid == 0) //no foreground group
+	if(fg_pid == 0) // no foreground group
  	   return;
-	kill(-fg_pid, SIGINT); //send SIGINT to the whole foreground group
+	kill(-fg_pid, SIGINT); // send SIGINT to the whole foreground group
 	errno = olderrno;
 }
-
-
 
 int main() {
     signal(SIGINT,  sigint_handler); // prevent ctrl + c
